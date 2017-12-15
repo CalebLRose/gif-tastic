54bee7c082d29097008ba2d9b0ad0300174d5e01
Caleb Rose <clrose86@gmail.com>
2017-12-14 21:21:50 -0600

    more javascript

--- assets/javascript/gif-api.js
+++ assets/javascript/gif-api.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	var q = "";
 	var btnArray = ["boomerang","astros","synthesizer","cats"];
 	var btn;
+	var offset;
 	// var x = 0;
 
 	function addBtn(){
@@ -11,7 +12,7 @@ $(document).ready(function(){
 		$("#premade-buttons").append(myBtn);
 	}
 	
-	for (var i=0;i<btnArray.length;i++){
+	for (i=0;i<btnArray.length;i++){
 		btn = btnArray[i];
 		addBtn();
 		// var myBtn = $("<button id=btn-"+i+">"+btnArray[i]+"</button>");
@@ -21,7 +22,7 @@ $(document).ready(function(){
 	};
 
 	function search(){
-		var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=aRKmO1b2giWVbaCzp30YdQ88s3NzPtnX&q="+q+"&limit=5&offset=0&rating=PG&lang=en"
+		var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=aRKmO1b2giWVbaCzp30YdQ88s3NzPtnX&q="+q+"&limit=6&offset="+offset+"&rating=PG-13&lang=en"
 		
 		$.ajax({
 			url: queryURL,
@@ -29,17 +30,35 @@ $(document).ready(function(){
 		})
 		.done(function(response){
 			console.log(response);
+			var results = response.data;
+			for (j=0;j<results.length;j++){
+				var gifsDiv = $("<div class='item'>");
+				var rating = results[j].rating;
+				var p = $("<p>").text("Rating: "+rating);
+				var gifImage = $("<img>");
+				gifImage.attr("src", results[j].images.fixed_width_still.url);
+				gifsDiv.prepend(p);
+				gifsDiv.prepend(gifImage);
+				$("#gifs").prepend(gifsDiv);
+			};
 		})
 	};
 
-	$("#new-gif").on("click",function(event){
+	$("#premade-buttons").on("click","button.my-btns",function(){
+		offset = Math.floor(Math.random()*100);
+		console.log("randomize: "+offset);
+		q = $(this).text();
+		console.log(q);
+		$(".item").remove()
+		search();
+	});
+
+		$("#new-gif").on("click",function(event){
 		event.preventDefault();
 		var input = $("input").val().trim();
 		console.log(input);
 		btn = input;
 		addBtn();
-	});
-
-	search();
+	});	
 
 })
